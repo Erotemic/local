2290c42a21dfa360f6cfa44257a13dfc5943dabf
Jon Crall <crallj@rpi.edu>
2014-01-08 13:46:35 -0500

    wip

--- init.sh
+++ init.sh
@@ -1,2 +1,6 @@
-ln -s ~/local/profile.sh .profile
+ln -s ~/local/profile.sh ~/.profile
+ln -s ~/local/vim/vimfiles ~/.vim
 
+echo "" > ~/.vimrc
+echo "source ~/local/vim/clean_vimrc" >> ~/.vimrc
+echo "source ~/local/vim/portable_vimrc" >> ~/.vimrc 
