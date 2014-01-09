79a77b80f3d7ea318edfec9763b1a8e2fc7f38ce
Jon Crall <crallj@rpi.edu>
2014-01-09 15:12:35 -0500

    profile wip

--- mac_profile.sh
+++ mac_profile.sh
@@ -1,3 +1,3 @@
 export PATH=$PATH:/opt/local/bin:/usr/local/lib:/usr/local/bin:/usr/local/include
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/local/lib/pkgconfig
-export PYTHON_PATH=$PYTHON_PATH:/usr/local/lib/python2.7/site-packages
+export PYTHON_PATH=$PYTHON_PATH:/usr/local/lib/python2.7/site-packages:/opt/local/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages/
