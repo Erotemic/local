c142881c580aacaad07cf6ac0becc07d17f3b635
Jon Crall <crallj@rpi.edu>
2014-01-09 13:57:41 -0500

    wip

--- init.sh
+++ init.sh
@@ -1,6 +1,13 @@
 ln -s ~/local/profile.sh ~/.profile
 ln -s ~/local/vim/vimfiles ~/.vim
 
+git config --global user.name joncrall
+git config --global user.email crallj@rpi.edu
+git config --global push.default simple
+
+
 echo "" > ~/.vimrc
 echo "source ~/local/vim/clean_vimrc" >> ~/.vimrc
 echo "source ~/local/vim/portable_vimrc" >> ~/.vimrc 
+
+
--- init_repos.sh
+++ init_repos.sh
@@ -7,3 +7,12 @@ git clone git@github.com:Erotemic/hotspotter.git
 git clone git@github.com:Erotemic/flann.git
 git clone git@github.com:Erotemic/hesaff.git
 git clone git@github.com:Erotemic/opencv.git
+
+sudo easy_install --upgrade setuptools
+sudo pip install setuptools --upgrade
+#sudo pip install matplotlib --upgrade
+sudo easy_install pylru
+#sudo pip install pylru
+
+
+sudo port install py27-matplotlib +qt4
