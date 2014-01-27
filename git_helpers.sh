c8a13dfb5933a649b9fe3a6b6070c5613ee8d954
Jon Crall <crallj@rpi.edu>
2014-01-27 09:14:48 -0500

    wip

--- git_helpers.sh
+++ git_helpers.sh
@@ -1,5 +1,19 @@
-
 git-recover()
 {
-git checkout $(git rev-list -n 1 HEAD -- "$1")^ -- "$1"
+    git checkout $(git rev-list -n 1 HEAD -- "$1")^ -- "$1"
+}
+
+git-mypull()
+{
+    cd ~/local
+    git pull
+    cd ~/code/hotspotter
+    git pull github jon
+    git pull 
+    cd ~/code/hesaff
+    git pull
+    cd ~/code/opencv
+    git pull
+    cd ~/code/flann
+    git pull
 }
