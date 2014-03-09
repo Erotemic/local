ffe45e92e4c423f22137e252997bea7542479a7f
Jon Crall <jon.crall@BakerStreet>
2014-03-09 16:22:53 -0400

    wip

--- freshstart/win7.bat
+++ freshstart/win7.bat
@@ -1,6 +1,12 @@
 :: This sets up the bare windows7 system
 :: with my basic preferences
 
+:: Installed drivers
+:: Installed chrome
+:: Installed dropbox
+
+:: Dropbox
+
 "https://github.com/Erotemic/local/archive/master.zip"
 
 set HOME=%USERPROFILE%
@@ -22,13 +28,19 @@ set GVIM_EXE="ftp://ftp.vim.org/pub/vim/pc/gvim74.exe"
 :: Install Python 
 wget "http://www.python.org/ftp/python/2.7.6/python-2.7.6.msi"
 wget "http://www.python.org/ftp/python/2.7.6/python-2.7.6.msi.asc"
-set PYMD5=ac54e14f7ba180253b9bae6635d822ea *python-2.7.6.msi
 
 md5sum python-2.7.6.msi > tmppymd5
 set /p PYMD5_VAL= < tmppymd5
 del tmppymd5
 
-if "%PYMD5%" NEQ "%PYMD5_VAL%" (goto :end)
+set PYMD5=ac54e14f7ba180253b9bae6635d822ea *python-2.7.6.msi
+
+if "%PYMD5%" NEQ "%PYMD5_VAL%" (
+    echo "md5 failed" 
+    goto :exit_fail
+    ) else (
+    echo "md5 passed"
+    )
 
 gpg --keyserver keys.gnupg.net
 gpg --recv-keys 7D9DC8D2 :: martin v lowis's key
@@ -76,7 +88,6 @@ set AHK_URL="http://l.autohotkey.net/AutoHotkey_L_Install.exe"
 :: Microsoft Security Essentials
 :: Zotero
 :: Dia
-:: Dropbox
 :: Flux
 :: Inno Setup 5
 :: LibreOffice
