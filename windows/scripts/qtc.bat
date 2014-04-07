f55eddee60998d8a50460e6a3a44b1c7b64bf6e7
joncrall <crallj@rpi.edu>
2014-04-07 16:32:06 -0400

    wip

--- windows/scripts/qtc.bat
+++ windows/scripts/qtc.bat
@@ -1,12 +1,15 @@
 :: cd %1
 :: @echo off
+:: NirCMD exec hide ipython qtconsole ^
 NirCMD exec hide ipython qtconsole ^
+ipython qtconsole ^
 --autocall 2 ^
 --colors=Linux ^
 --pylab=qt ^
+--matplotlib qt4 ^
 --ConsoleWidget.font_size=9 ^
 --ConsoleWidget.font_family="Mono Dyslexic" 
-
 ::^
-:b: --profile default 
+:: :b: --profile default 
+:: --profile default 
 :: @echo on
--- windows/scripts/sudo.bat
+++ windows/scripts/sudo.bat
@@ -1,4 +1,14 @@
 ::C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\Tools
 ::C:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\Tools\vsvars32.bat
 ::for /f "delims=" %%i in ('cd') do set cwd=%%i
-runas /profile /savecred /user:%COMPUTER_NAME%\%USERNAME% %1
+::
+
+
+:: Try1
+:: runas /profile /savecred /user:%COMPUTER_NAME%\%USERNAME% %1
+
+:: Try2
+@echo Set objShell = CreateObject("Shell.Application") > %temp%\sudo.tmp.vbs
+@echo args = Right("%*", (Len("%*") - Len("%1"))) >> %temp%\sudo.tmp.vbs
+@echo objShell.ShellExecute "%1", args, "", "runas" >> %temp%\sudo.tmp.vbs
+@cscript %temp%\sudo.tmp.vbs
