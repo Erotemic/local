ab96a2b4ad2161670f992f87cb4f9155fb168f49
Jon Crall <jon.crall@BakerStreet>
2014-07-24 10:25:37 -0400

    added vimfiles shortcut to gitignore

---
+++ windows/scripts/server.bat
@@ -0,0 +1,8 @@
+ssh jonathan@192.168.1.105
+
+::#echo -en "\033]0;SSH server\a"
+::#export SERVER_ADDRESS=ibeis.cs.uic.edu
+::#export SERVER_ADDRESS=131.193.42.63
+::ssh jonathan@$SERVER_ADDRESS
+::
+::ssh -X jonathan@192.168.1.178
