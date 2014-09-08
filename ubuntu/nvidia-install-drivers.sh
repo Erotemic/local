7588bd8f17bb1b5950e62e8fe47c116adbe8e597
Jon Crall <crallj@rpi.edu>
2014-09-08 15:15:20 -0400

    wip

--- ubuntu/nvidia-install-drivers.sh
+++ ubuntu/nvidia-install-drivers.sh
@@ -3,14 +3,21 @@
 
 # Remember you have to stop the xserver before you do anything else
 
+# ctrl+alt+f1
 # stop lightdm # Unity
 # stop gdm # Gnome3
 # stop kdm # KDE
 #/Drivers/NVIDIA-Linux-x86_64-319.32.run
 
+#http://www.nvidia.com/download/driverResults.aspx/77525/en-us
+#Geforce 670 GTX
+
 
 #export INSTALL_NVIDIA=/home/joncrall/Drivers/NVIDIA-Linux-x86_64-319.32.run
-export INSTALL_NVIDIA=/home/joncrall/Drivers/NVIDIA-Linux-x86_64-331.20.run
-ls -al $NVIDIA_DRIVER_INSTALLER*
+#export INSTALL_NVIDIA=/home/joncrall/Drivers/NVIDIA-Linux-x86_64-331.20.run
+mkdir ~/Drivers
+mv ~/Downloads/NVIDIA-Linux* ~/Drivers
+export INSTALL_NVIDIA=~/Drivers/NVIDIA-Linux-x86_64-340.32.run
+ls -al ~/Drivers
 chmod +x $INSTALL_NVIDIA
-$INSTALL_NVIDIA
+sudo $INSTALL_NVIDIA
