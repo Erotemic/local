f5eca1329107ff3d0fde224ca623339401be7414
joncrall <crallj@rpi.edu>
2014-10-14 17:10:49 -0400

    wip

--- init/nvidia-install-drivers.sh
+++ init/nvidia-install-drivers.sh
@@ -1,15 +1,3 @@
-# Install Correct Driver
-# apt-get install nvidia-current
-
-# Try this aptget repo
-sudo add-apt-repository ppa:xorg-edgers/ppa -y
-sudo apt-get update
-#sudo apt-get install nvidia-343
-#sudo apt-get remove nvidia-343
-sudo apt-get install nvidia-340
-
-
-
 # Remember you have to stop the xserver before you do anything else
 
 # ctrl+alt+f1
@@ -19,24 +7,16 @@ sudo apt-get install nvidia-340
 #/Drivers/NVIDIA-Linux-x86_64-319.32.run
 
 #http://www.nvidia.com/download/driverResults.aspx/77525/en-us
-#Geforce 670 GTX
 
+# Add drivers repo from edgers
+sudo add-apt-repository ppa:xorg-edgers/ppa -y
+sudo apt-get update
 
-#export INSTALL_NVIDIA=/home/joncrall/Drivers/NVIDIA-Linux-x86_64-319.32.run
-#export INSTALL_NVIDIA=/home/joncrall/Drivers/NVIDIA-Linux-x86_64-331.20.run
-manual_nvidia_install(){
-    mkdir ~/Drivers
-    mv ~/Downloads/NVIDIA-Linux* ~/Drivers
-    ls -al ~/Drivers
-
-    export INSTALL_NVIDIA=~/Drivers/NVIDIA-Linux-x86_64-340.32.run
-    chmod +x $INSTALL_NVIDIA
-    sudo stop gdm
-}
-# Doesnt actually seem echo correctly
-#sudo cat /etc/modprobe.d/blacklist.conf
-#sudo echo "blacklist nouveau" >> /etc/modprobe.d/blacklist.conf
-#sudo $INSTALL_NVIDIA
+# Install Correct Driver for Geforce 670 GTX
+# Use 340 for cuda compadability
+sudo apt-get install nvidia-340
+#sudo apt-get install nvidia-343
+#sudo apt-get remove nvidia-343
 
 install_cuda_prereq()
 {
@@ -68,9 +48,11 @@ install_cuda_prereq()
     sudo apt-get install -y python-pillow
 }
 
+install_cuda_prereq
+
 # Get the cuda 6.5 deb file
-mkdir ~/installers
-cd ~/installers
+mkdir ~/tmp
+cd ~/tmp
 wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_6.5-14_amd64.deb
 sudo dpkg -i cuda-repo-*
 # Carefull this removes 343 drivers and puts in 340 drivers
@@ -84,8 +66,51 @@ echo 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64:/usr/local/l
 source ~/.bashrc
 
 
+#TEST 
+
 nvcc --version
 nvidia-smi
+
+
+# INFO
+apt-cache depends cuda
+apt-cache depends nvidia-340-dev
+apt-cache depends nvidia-340-uvm
+
+
+# OTHER 
+
+reinstall_nvidia()
+{
+    sudo apt-get remove nvidia-340-dev
+    sudo apt-get remove nvidia-340
+    sudo apt-get remove cuda
+    sudo apt-get remove libcuda1-340
+    sudo apt-get remove nvidia-libopencl1-340
+    sudo apt-get remove nvidia-libopencl-icd-340
+    sudo apt-get remove nvidia-settings
+}
+
+# OLD
+# apt-get install nvidia-current
 # Use: "nvcc --version" for CUDA version [Ex: V6.5.X]
 # Use: "nvidia-smi" for driver version [Ex: 34X.XX]
 #sudo apt-get install nvidia-cuda-toolkit
+
+
+#export INSTALL_NVIDIA=/home/joncrall/Drivers/NVIDIA-Linux-x86_64-319.32.run
+#export INSTALL_NVIDIA=/home/joncrall/Drivers/NVIDIA-Linux-x86_64-331.20.run
+#manual_nvidia_install(){
+#    # USE APT-GET 340
+#    mkdir ~/Drivers
+#    mv ~/Downloads/NVIDIA-Linux* ~/Drivers
+#    ls -al ~/Drivers
+
+#    export INSTALL_NVIDIA=~/Drivers/NVIDIA-Linux-x86_64-340.32.run
+#    chmod +x $INSTALL_NVIDIA
+#    sudo stop gdm
+#}
+# Doesnt actually seem echo correctly
+#sudo cat /etc/modprobe.d/blacklist.conf
+#sudo echo "blacklist nouveau" >> /etc/modprobe.d/blacklist.conf
+#sudo $INSTALL_NVIDIA
