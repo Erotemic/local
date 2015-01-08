40718ffc10569c9be3f10053c835be2706c18c92
joncrall <erotemic@gmail.com>
2015-01-08 16:54:45 -0500

    wip

--- scripts/ubuntu_scripts/server.sh
+++ scripts/ubuntu_scripts/server.sh
@@ -4,9 +4,12 @@ echo -en "\033]0;SSH server\a"
 #export SERVER_ADDRESS=ibeis.cs.uic.edu
 #export SERVER_ADDRESS=131.193.42.63
 #export SERVER_ADDRESS=41.215.76.14
-export SERVER_ADDRESS=ibeis.cs.uic.edu
-echo "sshing into $SERVER_ADDRESS"
-ssh jonathan@$SERVER_ADDRESS
+#export SERVER_ADDRESS=ibeis.cs.uic.edu
+export SERVER_ADDRESS=41.203.223.178
+#export SERVER_PORT=22
+export SERVER_PORT=1022
+echo "sshing into $SERVER_ADDRESS at port $SERVER_PORT"
+ssh -p $SERVER_PORT jonathan@$SERVER_ADDRESS
 
 #-i ~/.ssh/id_rsa
 
