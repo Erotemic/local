acfe13ebb1974443a121b1acb927ec87dab18efc
joncrall <erotemic@gmail.com>
2015-02-11 09:36:16 -0500

    wip

--- init/ubuntu_core_packages.sh
+++ init/ubuntu_core_packages.sh
@@ -335,6 +335,28 @@ python /dev/fd/42 $@
 
 }
 
+install_nomachine()
+{
+    # https://www.nomachine.com/download/download&id=1
+
+    #utget http://download.nomachine.com/download/4.4/Linux/nomachine_4.4.6_8_i686.tar.gz
+    exec 42<<'__PYSCRIPT__'
+import utool as ut
+import os
+from os.path import join
+zipped_url = 'http://download.nomachine.com/download/4.4/Linux/nomachine_4.4.6_8_i686.tar.gz'
+unzipped_fpath = ut.grab_zipped_url(zipped_url)
+ut.vd(unzipped_fpath)
+os.chdir(unzipped_fpath)
+ut.cmd('./configure')
+ut.cmd('make')
+ut.cmd('cp -r --verbose NX /usr/NX', sudo=True)
+ut.cmd('/usr/NX/nxserver --install', sudo=True)
+__PYSCRIPT__
+python /dev/fd/42 $@
+
+}
+
 
 install_workrave()
 {
