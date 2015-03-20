c253dc4f5e1fcc06589e157266839a29102e975c
joncrall <erotemic@gmail.com>
2015-03-20 13:14:15 -0400

    wip

--- init/__REPOS1__.py
+++ init/__REPOS1__.py
@@ -88,7 +88,9 @@ VIM_REPO_URLS, VIM_REPOS = repo_list([
     'https://github.com/kien/rainbow_parentheses.vim.git',
     #'https://github.com/fholgado/minibufexpl.vim.git',
     'https://github.com/vim-scripts/taglist.vim.git',
-    'https://github.com/honza/vim-snippets.git'
+    'https://github.com/honza/vim-snippets.git',
+    'https://github.com/altercation/vim-colors-solarized.git',
+
 
     #'https://github.com/ggreer/the_silver_searcher.git'  # Ag
     # FOR SNIP MATE
