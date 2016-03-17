f35759e155a19c03edb5281e2542fd3ae1dcff0d
joncrall <erotemic@gmail.com>
2016-03-17 11:38:39 -0400

    wip

--- init/REPOS1.py
+++ init/REPOS1.py
@@ -29,7 +29,7 @@ IBEIS_REPOS_URLS, IBEIS_REPOS = repo_list([
     'https://github.com/Erotemic/hesaff.git',
     'https://github.com/Erotemic/guitool.git',
     #'https://github.com/aweinstock314/cyth.git',
-    'https://github.com/zmjjmz/ibeis-flukematch-module.git',
+    #'https://github.com/zmjjmz/ibeis-flukematch-module.git',
     'https://github.com/Erotemic/mtgmonte.git',
     'https://github.com/bluemellophone/ibeis_cnn.git',
     'https://github.com/Erotemic/plottool.git',
