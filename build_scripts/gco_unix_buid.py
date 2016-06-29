9ff7e66a68dd8e31780098bcbf48b04c6193d320
joncrall <erotemic@gmail.com>
2016-06-29 14:05:59 -0400

    wip

---
+++ build_scripts/gco_unix_buid.py
@@ -0,0 +1,19 @@
+curl -O http://147.228.240.61/queetech/install/gco-v3.0.zip
+
+# ORIG: https://github.com/amueller/gco_python
+
+ PYPI: https://pypi.python.org/pypi/pygco/0.0.10
+# PYPI (Linked): https://github.com/mjirik/gco_python
+
+# OTHER: https://github.com/yujiali/pygco
+
+# GCO: http://vision.csd.uwo.ca/code/
+
+# http://peekaboo-vision.blogspot.ca/2012/05/graphcuts-for-python-pygco.html
+
+
+wget http://vision.csd.uwo.ca/code/gco-v3.0.zip
+mkdir -p gco
+cp gco-v3.0.zip gco/gco-v3.0.zip
+cd gco
+7z x gco-v3.0.zip
