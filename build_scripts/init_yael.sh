865811ad890fe08e706e8dd54f83ae8a0e274ce1
joncrall <crallj@rpi.edu>
2016-10-05 00:15:44 -0400

    wip

--- build_scripts/init_yael.sh
+++ build_scripts/init_yael.sh
@@ -1,7 +1,28 @@
+export NCPUS=$(grep -c ^processor /proc/cpuinfo)
 cd $CODE_DIR 
 svn co svn://mygforgelogin@scm.gforge.inria.fr/svn/yael/trunk yael
+cd yael
 
-export NCPUS=$(grep -c ^processor /proc/cpuinfo)
+install_prereq()
+{
+    sudo apt-get install libblas-dev liblapack-dev
+    sudo apt-get install swig
+}
+
+git clone git@github.com:Erotemic/yael.git
+cd ~/code/yael
+./configure.sh --enable-numpy --msse4
+make
+
+
+python setup.py develop
+
+python -c "import yael; print(dir(yael))"
+python -c "from yael import yael; print(dir(yael)); print(yael.__package__)"
+python ~/code/yael/test/test_ctypes.py 
+python ~/code/yael/test/test_kmeans_alt_dist.py
+python ~/code/yael/test/py/test_ynumpy.py
+python ~/code/yael/test/py/test_numpy.py  
 
 
 download_url_repo()
@@ -40,7 +61,7 @@ export VERSION=yael_v401
 download_url_repo $URL $VERSION
 mv $VERSION ~/srcdistro 
 cd ~/srcdistro/$VERSION
-./configure
+./configure --msse4
 gmake -j$NCPUS
 #====================
 
