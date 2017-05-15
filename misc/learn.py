d64cd3b5206ace70173faa54eebbfcfe01852330
joncrall <crallj@rpi.edu>
2017-05-14 20:47:23 -0400

    wip

--- misc/learn.py
+++ misc/learn.py
@@ -41,3 +41,104 @@ def pandas_merge():
         with timer:
             new = pd.merge(a, b, how='outer', on=x.columns.tolist(),
                            left_index=True, right_index=True, copy=False)
+
+
+def ewma():
+    import plottool as pt
+    pt.qtensure()
+
+    # Investigate the span parameter
+    span = 20
+    alpha = 2 / (span + 1)
+
+    # how long does it take for the estimation to hit 0?
+    # (ie, it no longer cares about the initial 1?)
+    # about 93 iterations to get to 1e-4
+    # about 47 iterations to get to 1e-2
+    # about 24 iterations to get to 1e-1
+    # 20 iterations goes to .135
+    data = (
+        [1] +
+        [0] * 20 + [1] * 40 +
+        [0] * 20 + [1] * 50 +
+        [0] * 20 + [1] * 60 +
+        [0] * 20 + [1] * 165 +
+        [0] * 20 +
+        [0]
+    )
+    mave = []
+
+    iter_ = iter(data)
+    current = next(iter_)
+    mave += [current]
+    for x in iter_:
+        current = (alpha * x) + (1 - alpha) * current
+        mave += [current]
+
+    pt.figure(fnum=1, doclf=True)
+    pt.plot(data)
+    pt.plot(mave)
+
+    np.where(np.array(mave) < 1e-1)
+
+    import sympy as sym
+
+    # span, alpha, n = sym.symbols('span, alpha, n')
+    alpha = sym.symbols('alpha', real=True, nonnegative=True, finite=True)
+    n = sym.symbols('n', integer=True, nonnegative=True, finite=True)
+    span = sym.symbols('span', integer=True, nonnegative=True, finite=True)
+    # alpha = 2 / (span + 1)
+
+    current = 1
+    x = 0
+    steps = []
+    for _ in range(10):
+        current = (alpha * x) + (1 - alpha) * current
+        steps.append(current)
+
+    alpha = 2 / (span + 1)
+    at_time_n = (1 - alpha) ** n
+    from sympy.solvers.inequalities import solve_univariate_inequality
+    # Determine how to choose span, such that you get to .01 from 1
+    # in n timesteps
+    thresh_to_slope = []
+
+    for thresh in ut.ProgIter([.0001, .001, .01, .1, .2, .3, .4, .5]):
+        d = []
+        for s in range(5, 1000, 100):
+            a = solve_univariate_inequality(at_time_n.subs(span, s) < thresh, n)
+            d.append(a.lhs)
+        slope = np.diff(d).mean()
+        # pt.plot(d)
+        thresh_to_slope.append((thresh, slope))
+
+    pt.plt.plot(*zip(*thresh_to_slope), 'x-')
+
+    # for thresh=.01, we get a rough line with slop ~2.302,
+    # for thresh=.5, we get a line with slop ~34.66
+
+    # if we want to get to 0 in n timesteps, with a thresh of
+    # choose span=f(thresh) * (n + 2))
+    # f is some inverse exponential
+
+    # 0.0001, 460.551314197147
+    # 0.001, 345.413485647860,
+    # 0.01, 230.275657098573,
+    # 0.1, 115.137828549287,
+    # 0.2, 80.4778885203347,
+    # 0.3, 60.2031233261536,
+    # 0.4, 45.8179484913827,
+    # 0.5, 34.6599400289520
+
+    # Seems to be 4PL symetrical sigmoid
+    # f(x) = -66500.85 + (66515.88 - -66500.85) / (1 + (x/0.8604672)^0.001503716)
+    # f(x) = -66500.85 + (66515.88 - -66500.85)/(1 + (x/0.8604672)^0.001503716)
+
+    def f(x):
+        return -66500.85 + (66515.88 - -66500.85) / (1 + (x/0.8604672) ** 0.001503716)
+        # return (10000 * (-6.65 + (13.3015) / (1 + (x/0.86) ** 0.00150)))
+
+    # f(.5) * (n - 1)
+
+    # f(
+    solve_rational_inequalities(at_time_n < .01, n)
