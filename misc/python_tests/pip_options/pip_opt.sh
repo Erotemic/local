cfb84784581f52b92fcc2b507edc46a6c7fa8e82
joncrall <crallj@rpi.edu>
2017-10-20 10:06:54 -0400

    wip

--- misc/python_tests/pip_options/pip_opt.sh
+++ misc/python_tests/pip_options/pip_opt.sh
@@ -1,9 +1,17 @@
 
 
+# SPECIFYING ROOT SEEMS TO DO NOTHING TO INSTALL OF PACKAGE
 # IN VENV CASES INSTALL TO VENV
-pip install --root=$HOME/local/misc/python_tests/pip_options/root -e .
+pip install --root $HOME/local/misc/python_tests/pip_options/root -e .
 
-pip install --root=$HOME/local/misc/python_tests/pip_options/root -e .
+pip install --root $HOME/local/misc/python_tests/pip_options/root file://testpkg
+
+pip install --target $HOME/local/misc/python_tests/pip_options/target -e .
 
 pip install --root=root file://${VIAME_PACKAGES_DIR}/testpkg\#egg=testpkg[postgres]
 
+
+python -c "import testpkg.testmod; print(testpkg.__file__)"
+pip uninstall testpkg
+
+python -c "import testpkg.testmod; print(testpkg.__file__)"
