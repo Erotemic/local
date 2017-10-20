4ef36eca7dc697ec47f23d12e2177448628e0f98
joncrall <crallj@rpi.edu>
2017-10-20 10:25:46 -0400

    wip

--- misc/python_tests/pip_options/pip_opt.sh
+++ misc/python_tests/pip_options/pip_opt.sh
@@ -6,16 +6,41 @@ pip install --root $HOME/local/misc/python_tests/pip_options/root -e .
 pip install --root $HOME/local/misc/python_tests/pip_options/root file://subdir#egg=testpkg
 
 
-# INSIDE VENV: makes ./root/home/joncrall/venv3/... 
-pip install --root $HOME/local/misc/python_tests/pip_options/root ./subdir
 # OUTSIDE VENV: makes ./root/usr/local/lib/python3.5/dist-packages/testpkg
+pip install --root $HOME/local/misc/python_tests/pip_options/root ./subdir
+# INSIDE VENV: makes ./root/home/joncrall/venv3/... 
 pip install --root $HOME/local/misc/python_tests/pip_options/root ./subdir
 
 
-# INSIDE VENV: makes ./root/home/joncrall/venv3/... 
-pip install --target $HOME/local/misc/python_tests/pip_options/target ./subdir
 # OUTSIDE VENV: makes ./root/usr/local/lib/python3.5/dist-packages/testpkg
 pip install --target $HOME/local/misc/python_tests/pip_options/target ./subdir
+# INSIDE VENV: does the same thing. (VERY GOOD)
+pip install --target $HOME/local/misc/python_tests/pip_options/target ./subdir
+
+# OUTSIDE VENV: FAILS
+pip install --target $HOME/local/misc/python_tests/pip_options/target -e ./subdir
+# INSIDE VENV: FAILS
+pip install --target $HOME/local/misc/python_tests/pip_options/target -e ./subdir
+
+
+# OUTSIDE VENV: makes prefix/lib/python3.5/site-packages/testpkg
+pip install --install-option="--prefix=$HOME/local/misc/python_tests/pip_options/prefix" ./subdir
+# INSIDE VENV: same thing (VERY GOOD)
+pip install --install-option="--prefix=$HOME/local/misc/python_tests/pip_options/prefix" ./subdir
+
+
+# THE FOLLOWING WILL FAIL UNLESS YOU ADD TO YOUR PYTHON PATH (to support .pth files)
+export PYTHONPATH=$PYTHONPATH:$HOME/local/misc/python_tests/pip_options/prefix/lib/python3.5/site-packages
+
+# OUTSIDE VENV: FAILS
+pip install --install-option="--prefix=$HOME/local/misc/python_tests/pip_options/prefix" -e ./subdir
+# INSIDE VENV: FAILS
+pip install --install-option="--prefix=$HOME/local/misc/python_tests/pip_options/prefix" -e ./subdir
+
+
+rm -rf target/testpkg*
+rm -rf prefix/lib*
+rm -rf subdir/testpkg.egg-info/
 
 
 pip install ./subdir
