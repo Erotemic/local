5d36c4bba5de47b60a18cf7e7d24c2d057aa30a2
joncrall <crallj@rpi.edu>
2017-10-20 10:17:24 -0400

    wip

--- misc/python_tests/pip_options/pip_opt.sh
+++ misc/python_tests/pip_options/pip_opt.sh
@@ -3,15 +3,20 @@
 # SPECIFYING ROOT SEEMS TO DO NOTHING TO INSTALL OF PACKAGE
 # IN VENV CASES INSTALL TO VENV
 pip install --root $HOME/local/misc/python_tests/pip_options/root -e .
-
 pip install --root $HOME/local/misc/python_tests/pip_options/root file://subdir#egg=testpkg
-pip install file://subdir#egg=testpkg
 
 
 # INSIDE VENV: makes ./root/home/joncrall/venv3/... 
 pip install --root $HOME/local/misc/python_tests/pip_options/root ./subdir
 # OUTSIDE VENV: makes ./root/usr/local/lib/python3.5/dist-packages/testpkg
 pip install --root $HOME/local/misc/python_tests/pip_options/root ./subdir
+
+
+# INSIDE VENV: makes ./root/home/joncrall/venv3/... 
+pip install --target $HOME/local/misc/python_tests/pip_options/target ./subdir
+# OUTSIDE VENV: makes ./root/usr/local/lib/python3.5/dist-packages/testpkg
+pip install --target $HOME/local/misc/python_tests/pip_options/target ./subdir
+
 
 pip install ./subdir
 pip install -e subdir
