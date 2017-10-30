e9d580f36f4dc9487ae85e180afa16a1aa939e2e
joncrall <crallj@rpi.edu>
2017-10-29 22:19:29 -0400

    wip

--- scripts/dim.sh
+++ scripts/dim.sh
@@ -3,8 +3,12 @@
 #xrandr --output DVI-I-2 --brightness .3
 #xrandr --output DVI-I-3 --brightness .3
 
-#sudo apt-get install redshift
-#redshift -O 6500 -b 1.0
-#redshift -O 2500 -b .5
-redshift -O 2200 -b .4
-redshift -O 2000 -b .3
+if [ "$1" == "-u" ]; then
+    redshift -O 6500 -b 1.0
+else
+    #sudo apt-get install redshift
+    #redshift -O 6500 -b 1.0
+    #redshift -O 2500 -b .5
+    redshift -O 2200 -b .4
+    redshift -O 2000 -b .3
+fi
