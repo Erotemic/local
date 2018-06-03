67c8641bb85a195d0762dc456870357ffabfe621
joncrall <erotemic@gmail.com>
2018-06-03 18:12:52 -0400

    wip

--- git_tools/github_add_fork.py
+++ git_tools/github_add_fork.py
@@ -13,20 +13,32 @@ def github_add_fork(new_user, dry=False):
     parts = [p.split(' ')[0] for p in out.split('\n') if p]
     remote_to_url = dict([p.split('\t') for p in parts])
 
+    server = None
+
+    known_servers = [
+        'github.com',
+        'gitlab.com',
+    ]
+
     repo_names = set()
     for remote, url in remote_to_url.items():
         if new_user == remote:
             raise Exception('User already exists')
-        if 'github.com' in url:
-            user, gitname = url.split('github.com')[1][1:].split('/')
-            repo_names.add(gitname)
+        for _server in known_servers:
+            if _server in url:
+                user, gitname = url.split(_server)[1][1:].split('/')
+                repo_names.add(gitname)
+                server = _server
 
     if len(repo_names) != 1:
         raise Exception('Conflicting repo names')
 
     gitname = list(repo_names)[0]
 
-    new_url = 'https://github.com/{}/{}'.format(new_user, gitname)
+    if server is None:
+        raise Exception('Unknown server')
+
+    new_url = 'https://{}/{}/{}'.format(server, new_user, gitname)
     command = 'git remote add {} {}'.format(new_user, new_url)
     if not dry:
         ub.cmd(command, verbose=3)
