4b61feb7dfe04339561f0012c960b1b5b961a423
joncrall <jon.crall@kitware.com>
2018-07-20 11:51:32 -0400

    wip

--- homelinks/helpers/alias_helpers.sh
+++ homelinks/helpers/alias_helpers.sh
@@ -8,7 +8,7 @@
 alias pytree='tree -P "*.py" --dirsfirst'
 alias ls='ls --color --human-readable --group-directories-first --hide="*.pyc" --hide="*.pyo"'
 alias pygrep='grep -r --include "*.py"'
-alias clean_python='find . -iname *.pyc -delete && find . -iname *.pyo -delete && find . -iname __pycache__ -delete'
+alias clean_python='find . -iname __pycache__ -delete || find . -iname *.pyc -delete || find . -iname *.pyo -delete'
 
 # watch with a higher frequency
 alias watch='watch -n .5'
