9a999294689d60319a61a08d78fa95a8190c03ec
joncrall <jon.crall@kitware.com>
2022-03-31 00:30:02 -0400

    wip

--- build_scripts/build_btop.sh
+++ build_scripts/build_btop.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+__notes__='
+
+Get ubuntu version:
+    lsb_release -a”
+
+'
+
+__install_gcc11_on_2004__(){
+    # https://lindevs.com/install-gcc-on-ubuntu/
+    sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+    sudo apt install -y gcc-11 g++-11
+    gcc-11 --version
+    g++-11 --version
+}
+
 git_ensure(){
     GIT_URL=$1
     GIT_DPATH=$2
