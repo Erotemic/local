3f363d98778688a7fb774b517a78c14cf2088eee
joncrall <jon.crall@kitware.com>
2022-08-17 15:09:54 -0400

    wip

--- scripts/git-autoconf-gpgsign.sh
+++ scripts/git-autoconf-gpgsign.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 autoconfigure_local_git_autosign(){
     __heredoc_="
     Attempts to autoconfigure your local git repo to use the appropriate GPG
@@ -10,14 +12,22 @@ autoconfigure_local_git_autosign(){
     (3) you have a have an ssh key registered with the git-remote
     (4) you have a have an gpg key registered with the git-remote
     (5) that ssh key uses the same email as your gpg key (which should always be the case)
+
+    ~/local/scripts/git-autoconf-gpgsign.sh Erotemic
     "
 
     echo " --- Begin AutoConfigure GPGSign --- "
+    #git config --local --list | grep "gpg\|sign\|email"
 
-    git config --local --list | grep "gpg\|sign\|email"
+    REMOTE_PAT="$1"
 
     # First find which remote URL we are pushing to
-    REMOTE_URL=$(git remote -v | grep push | cut -d$'\t' -f2 | cut -d" " -f1)
+    if [[ "$REMOTE_PAT" == "" ]]; then
+        REMOTE_URL=$(git remote -v | grep push | cut -d$'\t' -f2 | cut -d" " -f1 | cut -d$'\n' -f1)
+    else
+        # If there is more than one remote, we may need the user to clarify it for us
+        REMOTE_URL=$(git remote -v | grep push | grep "$REMOTE_PAT" | cut -d$'\t' -f2 | cut -d" " -f1 | cut -d$'\n' -f1)
+    fi
     echo "REMOTE_URL = $REMOTE_URL"
 
     # List ssh identities
@@ -27,7 +37,7 @@ autoconfigure_local_git_autosign(){
     #GIT_SSH_COMMAND="ssh -v" git ls-remote $REMOTE_URL 
     #GIT_SSH_COMMAND="ssh -v" git ls-remote $REMOTE_URL 2>&1 >/dev/null | grep "identity file"
 
-    GIT_SSH_COMMAND="ssh -v" git ls-remote $REMOTE_URL 2> /tmp/my_ssh_stderr_file
+    GIT_SSH_COMMAND="ssh -v" git ls-remote "$REMOTE_URL" 2> /tmp/my_ssh_stderr_file
     PUBLIC_KEY_LINE=$(cat /tmp/my_ssh_stderr_file | grep "Offering public key")
     PUBLIC_KEY_LINE=$(python -c "print(chr(10).join([x for x in open('/tmp/my_ssh_stderr_file').read().split('\n') if 'identity file' in x][0:1]))")
     # note sure why a regular grep doesn't work
@@ -36,7 +46,7 @@ autoconfigure_local_git_autosign(){
     PRIVATE_KEY_FPATH=$(echo "$PUBLIC_KEY_LINE" | cut -d" " -f 4)
     # hack to find public key (not sure how robust this is)
     PUBLIC_KEY_FPATH="${PRIVATE_KEY_FPATH}.pub"
-    PUBLIC_KEY_EMAIL=$(cat $PUBLIC_KEY_FPATH | cut -d" " -f3)
+    PUBLIC_KEY_EMAIL=$(cat "$PUBLIC_KEY_FPATH" | cut -d" " -f3)
 
     # We found the public key email
     echo "
@@ -57,9 +67,9 @@ autoconfigure_local_git_autosign(){
     # https://help.github.com/en/articles/signing-commits
     git config --local commit.gpgsign true
     # Note the GPG key needs to match the email
-    git config --local user.email $UserEmail
+    git config --local user.email "$UserEmail"
     # Tell git which key to sign
-    git config --local user.signingkey $KEYID
+    git config --local user.signingkey "$KEYID"
 
     echo "
     CURRENT GLOBAL SETTINGS"
@@ -71,4 +81,4 @@ autoconfigure_local_git_autosign(){
 }
 
 
-autoconfigure_local_git_autosign
+autoconfigure_local_git_autosign "$@"
