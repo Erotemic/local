46eee0d6e9e4f8230d5509ec1c43879497dab579
joncrall <erotemic@gmail.com>
2022-08-25 13:53:36 -0400

    wip

---
+++ init/raids/setup_btrfs_raid.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+__doc__="""
+Based on benchmarks, it might be faster to use btrfs for a RAID-10 solution on
+an older machine.
+
+Benchmarks:
+    .. [FSBench2018] https://www.phoronix.com/review/freebsd-12-zfs
+    .. [FSBench2015] https://www.diva-portal.org/smash/get/diva2:822493/FULLTEXT01.pdf
+"""
