dbd8e3e55fba77a1e1d8c26142c2c6f359bfe33b
joncrall <erotemic@gmail.com>
2023-05-01 01:33:26 -0400

    wip

--- homelinks/helpers/alias_helpers.sh
+++ homelinks/helpers/alias_helpers.sh
@@ -146,7 +146,7 @@ alias wads='cd $(smartwatch_dvc --tags="phase2_data" --hardware="ssd")'
 alias wadh6='cd $(smartwatch_dvc --tags="phase2_data" --hardware="hdd")/Drop6'
 alias wads6='cd $(smartwatch_dvc --tags="phase2_data" --hardware="ssd")/Drop6'
 
-alias wads6m='cd $(smartwatch_dvc --tags="phase2_data" --hardware="ssd")/Drop6-MeanYear10GSD'
+alias wads6m='cd $(smartwatch_dvc --tags="phase2_data" --hardware="ssd")/Drop6-MeanYear10GSD-V2'
 
 alias wadf='cd $HOME/flash1/smart_watch_dvc'
 alias wau='cd $HOME/code/watch/watch/utils'
